2b83ada9d12423fc011bce2b1e7f4543599ebca3
toledkrw <matheus.tererj@hotmail.com>
2024-04-17 20:09:08 +0000

    🐛 fixed a bug on the post creation command

--- .devcontainer/post_creation.sh
+++ .devcontainer/post_creation.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-function check_credentials_file() {
+check_credentials_file() {
     if [ ! -d ".env" ]; then
-        echo "The '.env' folder does not exist. Now creating one:" &&
-        mkdir .env && 
-        touch .env/credentials.json && 
-        echo \
-        '{
-            "openai":"",
-            "gemini":""
-        }' > .env/credentials.json &&
-        echo "Done."
-    fi
-}
-function check_venv_folder() {
-    if [ ! -d ".venv" ]; then
-        echo "The '.venv' folder does not exist. Now creating one:" &&
-        poetry install &&
+        echo "The '.env' folder does not exist. Now creating one:"
+        mkdir .env
+        touch .env/credentials.json
+        echo -e '{\n\t"openai":"",\n\t"gemini":""\n}' > .env/credentials.json
         echo "Done."
     fi
 }
 
+check_venv_folder() {
+    if [ ! -d ".venv" ]; then
+        echo "The '.venv' folder does not exist. Now creating one:"
+        poetry install
+        echo "Done."
+    fi
+}
 
 check_credentials_file && check_venv_folder
