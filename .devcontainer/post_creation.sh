b12e4e6bb72e776830028858493606d2c74c801c
toledkrw <matheus.tererj@hotmail.com>
2024-05-20 22:42:03 -0300

    🧑‍💻 updated some .env stuff

--- .devcontainer/post_creation.sh
+++ .devcontainer/post_creation.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-check_credentials_file() {
-    if [ ! -d ".env" ]; then
-        echo "The '.env' folder does not exist. Now creating one:"
-        mkdir .env
-        touch .env/credentials.json
-        echo -e '{\n\t"openai":"",\n\t"gemini":""\n}' > .env/credentials.json
-        echo "Done."
-    fi
-}
-
 check_venv_folder() {
     if [ ! -d ".venv" ]; then
         echo "The '.venv' folder does not exist. Now creating one:"
@@ -18,4 +8,4 @@ check_venv_folder() {
     fi
 }
 
-check_credentials_file && check_venv_folder
+check_venv_folder
