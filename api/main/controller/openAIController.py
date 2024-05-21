c9ed4dddad312ea7756a9d17785f3c4b9b8378fb
toledkrw <matheus.tererj@hotmail.com>
2024-05-21 02:42:11 +0000

    🚧 changed opai controller to some dull controller

--- api/main/controller/openAIController.py
+++ api/main/controller/openAIController.py
@@ -1,31 +1,15 @@
 from flask import request, jsonify, Blueprint
-
 from openai import OpenAI
+import os
 
+MAIN_ROUTE = '/opai'
+CURRENT_MODEL = 'gpt-3.5-turbo-0125'
 
 openai_bp = Blueprint('openAI', __name__)
-MAIN_ROUTE = '/opai'
 
-client = OpenAI(api_key="credenciais")
+client = OpenAI(api_key=os.environ['OPENAI_KEY'])
 
 
 @openai_bp.route(MAIN_ROUTE + '/chat', methods=['POST'])
 def chat():
-    user_message = request.json.get('message')
-    
-    if not user_message:
-        return jsonify({'error': 'No message provided'}), 400
-
-    try:
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
-            messages=[
-                {"role": "system",
-                 "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-                {"role": "user", "content": user_message},
-            ]
-        )
-
-        return jsonify({'response': response.choices[0].message})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    pass
