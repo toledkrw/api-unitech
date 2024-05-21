cc88962afdeb2a9ec0b470f7bef3ffa5c98dd727
toledkrw <matheus.tererj@hotmail.com>
2024-05-21 02:42:40 +0000

    ✨ added 'generate multiple choices question' via gemini

--- api/main/controller/geminiAIController.py
+++ api/main/controller/geminiAIController.py
@@ -1,51 +1,44 @@
 from flask import request, jsonify, Blueprint
 
 import google.generativeai as genai
-import io, PIL.Image
+import os
 
 
 gemini_bp = Blueprint('geminiAI', __name__)
 MAIN_ROUTE = '/gmni'
 
-genai.configure(api_key="credenciais")
+genai.configure(api_key=os.environ['GEMINI_KEY'])
 
 
-@gemini_bp.route(MAIN_ROUTE + '/analisar_codigo', methods=['POST'])
-def gerar_resposta():
-    dados = request.get_json()
-
-    if 'code' not in dados or not isinstance(dados['code'], str):
-        return jsonify({'erro': 'Por favor, forneça uma chave "pergunta" válida como uma string.'}), 400
-
-    code = dados['code']
-    prompt = dados['prompt']
+@gemini_bp.route(MAIN_ROUTE + '/multiple_choice_question', methods=['POST'])
+def generate_multiple_choice_question():
+    data = request.get_json()
 
     try:
+        if 'subject' not in data:
+            raise ValueError('"Subject was not given".')
+        if 'academic_level' not in data:
+            raise ValueError('"Academic level was not given".')
+        if 'theme' not in data:
+            raise ValueError('"Theme was not given".')
+        
+        subject = data['subject']
+        academic_level = data['academic_level']
+        theme = data['theme']
+
+        locale = 'pt-br' if 'locale' not in data else data['locale']
+
+        prompt=\
+            f"Generate a test question, with five options (A, B, C, D, E), about the subject of {subject} at the {academic_level} level, focusing on {theme} theme." + \
+            "Please provide your response in Json format with the following structure: " + \
+            "{question_statement:'question statement that you will generate', alternatives:[{a:'option'}, {b:'option'}, {c:'option'}, {d:'option'}, {e:'option'}]], correct_answer:'correct_option_letter'}" + \
+            f"The question statement and alternatives must be in the following language: {locale}."
+        
         model = genai.GenerativeModel('gemini-pro')
-
-        response = model.generate_content(prompt + code)
-
-        return jsonify({'response': response.text})
-    except Exception as e:
-        return jsonify({'erro': f"Ocorreu um erro ao processar a solicitação: {str(e)}"}), 500
-    
-@gemini_bp.route(MAIN_ROUTE + '/gerar_imagem', methods=['POST'])
-def gerar_imagem():
-    try:
-        uploaded_file = request.files['image']
-        uploaded_text = request.form['text']
-
-        img = PIL.Image.open(uploaded_file)
-        image_bytes = io.BytesIO()
-        img.save(image_bytes, format='PNG')  # Altere o formato conforme necessário
-        image_bytes.seek(0)
-
-        model = genai.GenerativeModel('gemini-pro-vision')
-
-        response = model.generate_content([uploaded_text, img])
-        response.resolve()
+        response = model.generate_content(prompt)
 
         return jsonify({'response': response.text})
-
+    except ValueError as e:
+        return jsonify({'Error! ': f"An Error ocurred while processing your request: {str(e)}"}), 400
     except Exception as e:
-        return jsonify({'error': f"Ocorreu um erro ao processar a solicitação: {str(e)}"}), 500
+        return jsonify({'Error! ': f"An Error ocurred while processing your request: {str(e)}"}), 500
